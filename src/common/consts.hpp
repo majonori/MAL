b4e3806832cfbbdacb7e53d2c2112af8d8f28db8
MatchaParfait222 <3905776658@qq.com>
2026-07-27 14:32:18 +0800

    Consts

---
+++ src/common/consts.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include <complex>
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cpx = std::complex<double>;
+using std::vector;
+
+/* Edited on 2026/07/27
+This is a public definition.
+*/
